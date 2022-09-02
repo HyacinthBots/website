919611f3e172cd83c4800a80677bcb9282b29292
NoComment1105 <nocomment1105@outlook.com>
2022-09-02 13:47:12 +0100

    Update theme and add image

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -7,6 +7,20 @@ module.exports = {
     description: 'Discord bots, open source, free, forever.',
 
     theme: defaultTheme({
+            logo: 'https://raw.githubusercontent.com/HyacinthBots/art/main/branding/icons/Hyacinth%20transparent%20icon.png',
+            toggleColorMode: "Toggle color mode",
+            toggleSidebar: "Toggle sidebar",
+            contributors: false,
+            notFound: ["The page you are looking for does not exist or has been moved!"],
+
+            editLink: true,
+            editLinkText: "Help improve this page on GitHub",
+            editLinkPattern: ":repo/edit/:branch/:path",
+
+            docsRepo: 'https://github.com/HyacinthBots/website',
+            docsBranch: 'main',
+            docsDir: 'docs',
+
             navbar: [
                 {
                     text: 'Home',
