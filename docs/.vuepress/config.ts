0457e8ffeae834f6748558179a2de4ab96257d75
nocomment <nocomment1105@outlook.com>
2023-01-22 17:42:01 +0000

    Clean up config.ts and remove un-needed plugin stuff

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -1,96 +1,91 @@
-import {defineUserConfig} from "@vuepress/cli";
 import {defaultTheme} from "vuepress";
-import {backToTopPlugin} from "@vuepress/plugin-back-to-top";
 
 
-export default defineUserConfig({
+export default {
     base: '/',
     lang: 'en-US',
     title: 'HyacinthBots',
     description: 'Discord bots, open source, free, forever.',
 
     theme: defaultTheme({
-            logo: 'https://raw.githubusercontent.com/HyacinthBots/art/main/branding/icons/Hyacinth%20transparent%20icon.png',
-            toggleColorMode: 'Toggle color mode',
-            toggleSidebar: 'Toggle sidebar',
-            contributors: false,
-            notFound: ['The page you are looking for does not exist or has been moved!'],
+		logo: 'https://raw.githubusercontent.com/HyacinthBots/art/main/branding/icons/Hyacinth%20transparent%20icon.png',
+		toggleColorMode: 'Toggle color mode',
+		toggleSidebar: 'Toggle sidebar',
+		contributors: true,
+		notFound: ['The page you are looking for does not exist or has been moved!'],
 
-            editLink: true,
-            editLinkText: 'Help improve this page on GitHub',
-            editLinkPattern: ':repo/edit/:branch/:path',
+		editLink: true,
+		editLinkText: 'Improve this page on GitHub',
+        editLinkPattern: ':repo/edit/:branch/:path',
 
-            docsRepo: 'https://github.com/HyacinthBots/website',
-            docsBranch: 'main',
-            docsDir: 'docs',
+		repo: 'https://github.com/HyacinthBots/',
+		docsRepo: 'https://github.com/HyacinthBots/website',
+		docsBranch: 'main',
+		docsDir: 'docs',
 
-            navbar: [
-                {
-                    text: 'Home',
-                    link: '/'
-                },
-                {
-                    text: 'About',
-                    link: '/about.md'
-                },
-                {
-                    text: 'Bots',
-                    children: [
-                        {
-                            text: "Lily",
-                            link: '/bots/lily/about-lily.md'
-                        },
-                        {
-                            text: "Watchdog",
-                            link: '/bots/watchdog/about-watchdog.md'
-                        }]
-                }
-            ],
+		navbar: [
+			{
+				text: 'Home',
+				link: '/'
+			},
+			{
+				text: 'About',
+				link: '/about.md'
+			},
+			{
+				text: 'Bots',
+				children: [
+					{
+						text: "Lily",
+						link: '/bots/lily/about-lily.md'
+					},
+					{
+						text: "Watchdog",
+						link: '/bots/watchdog/about-watchdog.md'
+					}
+				]
+			}
+		],
 
-            sidebar: [
-                {
-                    text: 'Home',
-                    link: '/',
-                },
-                {
-                    text: 'About',
-                    link: '/about.md'
-                },
-                {
-                    text: 'Bots',
-                    link: '/bots.md',
-                    children: [
-                        {
-                            text: 'Lily',
-                            link: '/bots/lily/about-lily.md',
-                            children: [
-								{
-									text: 'Commands',
-									link: '/bots/lily/commands.md'
-								},
-                                {
-                                    text: 'GitHub',
-                                    link: 'https://github.com/HyacinthBots/LilyBot'
-                                }
-                            ]
-                        },
-                        {
-                            text: 'Watchdog',
-                            link: '/bots/watchdog/about-watchdog.md',
-                            children: [
-                                {
-                                    text: 'GitHub',
-                                    link: 'https://github.com/HyacinthBots/watchdog'
-                                }
-                            ]
-                        }
-                    ]
-                }
-            ]
-        }
-    ),
-
-    plugins: [
-        backToTopPlugin()
-    ]
-})
+		sidebar: [
+			{
+				text: 'Home',
+				link: '/',
+			},
+			{
+				text: 'About',
+				link: '/about.md'
+			},
+			{
+				text: 'Bots',
+				link: '/bots.md',
+				children: [
+					{
+						text: 'Lily',
+						link: '/bots/lily/about-lily.md',
+						children: [
+							{
+								text: 'Commands',
+								link: '/bots/lily/commands.md'
+							},
+							{
+								text: 'GitHub',
+								link: 'https://github.com/HyacinthBots/LilyBot'
+							}
+						]
+					},
+					{
+						text: 'Watchdog',
+						link: '/bots/watchdog/about-watchdog.md',
+						children: [
+							{
+								text: 'GitHub',
+								link: 'https://github.com/HyacinthBots/watchdog'
+							}
+						]
+					}
+				]
+			}
+		]
+	}),
+}
